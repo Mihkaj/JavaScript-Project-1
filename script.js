13e0960c92d46b8cffa57d01fa25090fa64b0091
Mihkaj <mihka.jh02@gmail.com>
2026-03-10 11:04:39 +0100

    feat: Add introductory console logs and refactor division by zero error handling to throw an error.

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Mini Calculator
 
-function add(a, b) {
-    return a + b;
-}
+console.log("Welcome to my calc! (That's slang fot calculator).")
+console.log("Just type in a number, then an Operator(+,-,*,/), then another number.")
+console.log("Then be prepared to witness mathematical magic!")
 
-function subtract(a, b) {
-    return a - b;
-}
 
-function multiply(a, b) {
-    return a * b;
-}
 
-function devide(a, b) {
-   
-    if (b === 0) {
-        return "Error: Cannot divide by zero";
-    } else 
+    function add(a, b) {
+        return a + b;
+    }
+
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function devide(a, b) {
         return a / b;
- }
-   
+    }
